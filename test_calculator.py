78759d099ad62e769c66b960237ca65f9a2bda0f
Pradeepak5 <pradeepak571@gmail.com>
2022-03-25 11:54:12 +0530

    setUp and tearDown updated

--- test_calculator.py
+++ test_calculator.py
@@ -1,31 +1,34 @@
+import sys
+
 import calculator
 
 import unittest
-
 class calculator_test(unittest.TestCase):
+
+    def setUp(self):
+        self.a=20
+        self.b=10
+
+    def tearDown(self):
+        self.a=0
+        self.b=0
+
     def test_add(self):
-        a=20
-        b=10
-        c=calculator.add(a,b)
-        self.assertEqual(a+b,c)
+        c=calculator.add(self.a,self.b)
+        self.assertEqual(self.a+self.b,c)
 
     def test_sub(self):
-        a=20
-        b=10
-        c=calculator.sub(a,b)
-        self.assertEqual(a-b,c)
+        c=calculator.sub(self.a,self.b)
+        self.assertEqual(self.a-self.b,c)
 
     def test_mul(self):
-        a=20
-        b=10
-        c=calculator.Mul(a,b)
-        self.assertEqual(a*b,c)
+        c=calculator.Mul(self.a,self.b)
+        self.assertEqual(self.a*self.b,c)
 
+    @unittest.skipIf(sys.platform.startswith("win"), "requires NOT Windows os")
     def test_div(self):
-        a=20
-        b=10
-        c=calculator.div(a,b)
-        self.assertEqual(a/b,c)
+        c=calculator.div(self.a,self.b)
+        self.assertEqual(self.a/self.b,c)
 
 if __name__=="__main__":
     unittest.main()
